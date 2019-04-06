e766fd61b7887104d9e45ba4d7d287499098726f
Jose Nunes <josenunes34@gmail.com>
2019-04-06 13:51:50 +0100

    WIDY-57 Update snapshots

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   linters: {
-    '**/*.+(js|jsx|md)': ['eslint --fix', 'prettier --write', 'jest --findRelatedTests', 'git add'],
+    '**/*.+(js|jsx|md)': ['eslint --fix', 'prettier --write', 'git add'],
   },
 };
