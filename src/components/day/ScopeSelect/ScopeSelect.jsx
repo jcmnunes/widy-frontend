4512740f47c443cc4b51af727dd98415eff28368
Jose Nunes <josenunes34@gmail.com>
2020-01-07 12:04:23 +0000

    feat: Improve create new scope functionality
    WDY-244

--- src/components/day/ScopeSelect/ScopeSelect.jsx
+++ src/components/day/ScopeSelect/ScopeSelect.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Select } from '@binarycapsule/ui-capsules';
+import { Button, Select } from '@binarycapsule/ui-capsules';
 import CustomLabel from './components/CustomLabel';
 import { scopesOptionsSelector, scopesSelector } from '../../../selectors/scopes/scopesSelectors';
 import { selectedTaskSelector } from '../../../selectors/tasks/tasksSelectors';
 import { updateTask } from '../../../actions/tasks';
+import ScopeModal from '../../settings/Page/Scopes/ScopeModal/ScopeModal';
 
 const ScopeSelect = () => {
+  const [isScopeModalOpen, setIsScopeModalOpen] = useState(false);
+
   const scopesOptions = useSelector(scopesOptionsSelector);
   const task = useSelector(selectedTaskSelector);
   const scopes = useSelector(scopesSelector);
@@ -25,16 +28,28 @@ const ScopeSelect = () => {
   };
 
   return (
-    <Select
-      isClearable
-      value={scope ? { id: scope.id, label: scope.name, shortCode: scope.shortCode } : null}
-      options={scopesOptions}
-      placeholder="No scope"
-      onChange={opt => dispatch(updateTask(task.id, { scopeId: opt ? opt.value : null }))}
-      formatOptionLabel={CustomLabel}
-      menuPortalTarget={document.body}
-      filterOption={filterScopes}
-    />
+    <>
+      <Select
+        isClearable
+        value={scope ? { id: scope.id, label: scope.name, shortCode: scope.shortCode } : null}
+        options={scopesOptions}
+        placeholder="No scope"
+        onChange={opt => dispatch(updateTask(task.id, { scopeId: opt ? opt.value : null }))}
+        formatOptionLabel={CustomLabel}
+        menuPortalTarget={document.body}
+        filterOption={filterScopes}
+      />
+      <Button onClick={() => setIsScopeModalOpen(true)} appearance="none" size="small">
+        + Create new scope
+      </Button>
+
+      {isScopeModalOpen && (
+        <ScopeModal
+          closeModal={() => setIsScopeModalOpen(false)}
+          onCreateScope={({ id }) => dispatch(updateTask(task.id, { scopeId: id }))}
+        />
+      )}
+    </>
   );
 };
 
